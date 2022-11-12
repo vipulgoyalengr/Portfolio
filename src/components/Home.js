483556af51692057b284769b2a4d57f87a21ed97
vipulgoyalengr <vipulgoyal.engr@gmail.com>
2022-11-12 22:31:38 +0530

    second commit
    second commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,14 +7,17 @@ const Home = () => {
         <div className='col-md-6 d-flex justify-content-center'>
         <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar"/>
         </div>
-        <div className='col-md-6 col11' >
+        <div className='col-md-6 col11 ' >
     <h1>Hi, I'm <p className='name' style={{display:'inline'}}>Vipul Goyal</p></h1>
     <h3>I'm a Full Stack Developer</h3>
     
-    <span class="fa-li"><i class="bi bi-9-circle mx-3"></i></span>List icons can
-    
-    <div><i class="bi bi-linkedin mx-3"></i><i class="bi bi-github mx-3"></i></div>
- 
+    <span class="fa-li" style={{fontSize:"calc(3px + 2vmin)"}}><i class="bi bi-briefcase-fill mx-3" style={{color:"#f76f00"}}></i> Developer at TCS</span>
+    <span class="fa-li " style={{fontSize:"calc(3px + 2vmin)"}}><i class="bi bi-geo-alt-fill mx-3" style={{color:"#f00a0a"}}></i> Jaipur, Rajasthan,India</span>
+    <span class="fa-li " style={{fontSize:"calc(3px + 2vmin)"}}><i class="bi bi-envelope-fill mx-3" style={{color:"#ffffff"}}></i> vipulgoyal.engr@gmail.com</span>
+
+    <div className='my-3'><a href="https://www.linkedin.com/in/vipul-goyal-861194186" target="_blank"><i class="bi bi-linkedin mx-3" style={{fontSize:"40px"}}></i></a><a href='https://github.com/vipulgoyalengr' target="_blank"><i class="bi bi-github mx-3" style={{fontSize:"40px"}}></i></a>
+    {/* <i class="bi bi-instagram mx-3"></i> */}
+    </div>
         </div>
       </div>
 
