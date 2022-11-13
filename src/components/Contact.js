ea2f81d4d680f8c35dcc15b827128bc0e9077e81
vipulgoyalengr <vipulgoyal.engr@gmail.com>
2022-11-13 09:46:48 +0530

    third commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,8 +7,8 @@ const Contact = () => {
     <hr className='mx-5'></hr>
       <div className='container c3 '>
     <div className='c4' >
-    <span class="fa-li" style={{fontSize:"40px"}}><i class="bi bi-envelope mx-3" ></i>: <a href="mailto:vipulgoyal.engr@gmail.com">vipulgoyal.engr@gmail.com</a></span><br/>
-      <span class="fa-li" style={{fontSize:"40px"}}><i class="bi bi-telephone mx-3" ></i>: <a href="tel:+917230022389">+91 7230022389</a></span>
+    <span class="fa-li" style={{fontSize:"calc(10px + 2vmin)"}}><i class="bi bi-envelope mx-3" ></i>: <a href="mailto:vipulgoyal.engr@gmail.com">vipulgoyal.engr@gmail.com</a></span><br/>
+      <span class="fa-li" style={{fontSize:"calc(10px + 2vmin)"}}><i class="bi bi-telephone mx-3" ></i>: <a href="tel:+917230022389">+91 7230022389</a></span>
     </div>
 
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import '../components/Home.css'
 const Home = () => {
   return (
-    <div className='container c1' >
+    <div className=' c1 ' >
       <div className='row'>
         <div className='col-md-6 d-flex justify-content-center'>
         <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar"/>
@@ -11,9 +11,9 @@ const Home = () => {
     <h1>Hi, I'm <p className='name' style={{display:'inline'}}>Vipul Goyal</p></h1>
     <h3>I'm a Full Stack Developer</h3>
     
-    <span class="fa-li" style={{fontSize:"calc(3px + 2vmin)"}}><i class="bi bi-briefcase-fill mx-3" style={{color:"#f76f00"}}></i> Developer at TCS</span>
-    <span class="fa-li " style={{fontSize:"calc(3px + 2vmin)"}}><i class="bi bi-geo-alt-fill mx-3" style={{color:"#f00a0a"}}></i> Jaipur, Rajasthan,India</span>
-    <span class="fa-li " style={{fontSize:"calc(3px + 2vmin)"}}><i class="bi bi-envelope-fill mx-3" style={{color:"#ffffff"}}></i> vipulgoyal.engr@gmail.com</span>
+    <span class="fa-li" style={{fontSize:"calc(7px + 2vmin)"}}><i class="bi bi-briefcase-fill mx-3" style={{color:"#f76f00"}}></i> Developer at TCS</span>
+    <span class="fa-li " style={{fontSize:"calc(7px + 2vmin)"}}><i class="bi bi-geo-alt-fill mx-3" style={{color:"#f00a0a"}}></i> Jaipur, Rajasthan,India</span>
+    <span class="fa-li " style={{fontSize:"calc(7px + 2vmin)"}}><i class="bi bi-envelope-fill mx-3" style={{color:"#ffffff"}}></i> vipulgoyal.engr@gmail.com</span>
 
     <div className='my-3'><a href="https://www.linkedin.com/in/vipul-goyal-861194186" target="_blank"><i class="bi bi-linkedin mx-3" style={{fontSize:"41px"}}></i></a><a href='https://github.com/vipulgoyalengr' target="_blank"><i class="bi bi-github mx-3" style={{fontSize:"41px"}}></i></a>
     {/* <i class="bi bi-instagram mx-3"></i> */}
